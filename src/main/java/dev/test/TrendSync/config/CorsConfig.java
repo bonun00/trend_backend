1bf8c51e59daa8bce764e6c48afd7bc93b4a06f9
bonun00 <lifesciences00@naver.com>
2025-03-28 16:13:06 +0900

    cors 변경

--- src/main/java/dev/test/TrendSync/config/CorsConfig.java
+++ src/main/java/dev/test/TrendSync/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
 //                        .allowedOrigins("http://localhost:3000") // React 등 프론트엔드에서 접근 가능하도록 허용
-                        .allowedOrigins("http://192.168.0.57:3004")
+                        .allowedOrigins("http://192.168.0.57:7391")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
